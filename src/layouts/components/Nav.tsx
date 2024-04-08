637aa7b4dd10bc879aeb682f479747a3a1ee1bfd
feiyang1020 <feiyangl1020@163.com>
2024-04-08 17:20:36 +0800

    fix:add telegram

--- src/layouts/components/Nav.tsx
+++ src/layouts/components/Nav.tsx
@@ -1,5 +1,6 @@
 import github from "@/assets/github.svg";
 import x from "@/assets/x.svg";
+import tg from "@/assets/tg.svg";
 import medium from "@/assets/medium.svg";
 import discord from "@/assets/discord.svg";
 import instagram from "@/assets/instagram.svg";
@@ -13,6 +14,17 @@ const items = [
     icon: x,
     link: "https://twitter.com/OctopusSpaceX",
     label: "x",
+  },
+  {
+    icon: tg,
+    link: "https://t.me/OctopusSpaceGlobal",
+    label: "telegram",
+    className:"telegram"
+  },{
+    icon: tg,
+    link: "https://t.me/OctopusSpace_cn",
+    label: "telegram2",
+    className:"telegram"
   },
   // {
   //   icon: medium,
@@ -35,8 +47,8 @@ export default () => {
   return (
     <div className="nav">
       {items.map((item) => (
-        <a key={item.label} target="_blank" href={item.link}>
-          <img src={item.icon} alt={item.label} className="navIcon" />
+        <a key={item.label} target="_blank" href={item.link} className="navItem">
+          <img src={item.icon} alt={item.label} className={item.className} />
         </a>
       ))}
     </div>
